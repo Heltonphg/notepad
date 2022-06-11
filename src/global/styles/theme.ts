d4a5d36af190ea3c60fcd53935d0ee72355bda05
heltonwylammicostapinto <wylammi.helton@gmail.com>
2022-06-11 10:14:04 -0300

    ✨ styles global

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,26 @@
+import { ms } from 'react-native-size-matters';
+
+export default {
+	colors: {
+		primary: '#5336D3',
+		text_dark: '#000000',
+		background: '#F5F5F5',
+	},
+
+	fonts: {
+		regular: 'BalooBhai2_400Regular',
+		medium: 'BalooBhai2_500Medium',
+		semi_bold: 'BalooBhai2_600SemiBold',
+		bold: 'BalooBhai2_700Bold',
+	},
+
+	metrics: {
+		tiny: ms(12),
+		small: ms(14),
+		normal: ms(16),
+		big: ms(18),
+		base_padding: ms(10),
+		base_margin: ms(10),
+		base_border_radius: ms(5),
+	},
+};
