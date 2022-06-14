5d2fcdd5eb03cc63f64f6cad8fd5939ca326a92d
heltonwylammicostapinto <wylammi.helton@gmail.com>
2022-06-14 02:10:48 -0300

    🚧 base persist storage

---
+++ src/utils/base_async_storage.ts
@@ -0,0 +1,31 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const EVENTS_KEY = '@notepad';
+
+interface BaseAsyncStorageProps {
+	key: string;
+	value: any;
+}
+
+export const persistStorage = async <T>(data: BaseAsyncStorageProps) => {
+	try {
+		return AsyncStorage.setItem(
+			`${EVENTS_KEY}:${data.key}`,
+			JSON.stringify(data.value),
+		);
+	} catch (e) {
+		console.warn('AsyncStorage:setItem', e);
+	}
+};
+
+export const loadStorage = async <T>(key: string): Promise<T | null> => {
+	try {
+		const value = await AsyncStorage.getItem(`${EVENTS_KEY}:${key}`);
+		if (value !== null) {
+			return JSON.parse(value);
+		}
+	} catch (e) {
+		console.warn('AsyncStorage:getItem', e);
+	}
+	return Promise.resolve(null);
+};
